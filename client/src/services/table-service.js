84ea068c0f64cda135b7682f817937c0989da58c
Fuko <fukodiaz@gmail.com>
2023-01-30 23:47:17 +0700

    corrections for deployment

--- client/src/services/table-service.js
+++ client/src/services/table-service.js
@@ -1,7 +1,8 @@
 export default class TableService {
 
 	// _apiBase= 'http://localhost:3000';
-	_apiBase= 'https://table-test-spa.herokuapp.com/api';
+	//_apiBase= 'https://table-test-spa.herokuapp.com/api';
+	_apiBase= 'https://table-test-api.vercel.app/api';
 
 	getResource = async (url) => {
 		const res = await fetch(`${this._apiBase}${url}`);
